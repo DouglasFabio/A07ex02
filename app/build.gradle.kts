7e9f7852a161cd362d59428c101a3c97aa295053
Douglas Fabio <douglasfabio.ifsp@gmail.com>
2023-12-08 09:09:04 -0300

    A07ex02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.a07ex02"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a07ex02"
--- app/src/main/java/com/example/a07ex02/MainActivity.kt
+++ app/src/main/java/com/example/a07ex02/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.a07ex02
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var meuLayout = findViewById<ConstraintLayout>(R.id.telaPrincipal)
+
+        meuLayout.setOnClickListener { view ->
+            var rotulo = "clicou"
+            var s = Snackbar.make(view, rotulo.toString(), Snackbar.LENGTH_INDEFINITE )
+            s.setAction("Fechar", {s.dismiss()})
+            s.show()
+        }
     }
 }
